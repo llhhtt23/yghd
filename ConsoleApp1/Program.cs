a03304bd9661664602c750bc9ac6bafb40eb7d0f
Your Name <you@example.com>
2026-08-03 20:32:41 +0800

    版本1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel;
+using System.Threading.Channels;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1------------------------------
+            //    int a = 2;
+            //    int b = 4;
+            //    Console.WriteLine(a+b);
+            //2------------------------------
+            //double c = l0;
+            //double k = 5 / 9.0 * (c - 32);
+            //Console.WriteLine($"华式摄氏度:{c:F3}度,摄氏度:{k:F3}度");
+            //3---------------------------------
+
+            //int a = 3;
+            //int b = 4;
+            //int c = b;//c=4
+            // b= a;//b=3
+            //a=c;//a=4
+            //Console.WriteLine($"a={a},b={b}");
+
+            //4-------------------------------
+            //double num = 89 / 24;
+            //double day = Math.Round(num);
+            //double h = 80 % 24;
+            //Console.WriteLine($"{day}天{ h}小时");
+            
+        }
+    }
+
+}
