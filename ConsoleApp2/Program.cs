787ff9df9c8449a8528c9b2dddabe208697db62c
Your Name <you@example.com>
2026-08-04 18:40:45 +0800

    2

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.ComponentModel;
+using System.Threading.Channels;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> arr1 = new List<string> 
+            //{ "A", "B", "C", "D" };
+            //Console.WriteLine(arr1.Count);
+            //arr1.Add("Z");
+            //arr1.Insert(2, "X");
+            //arr1.AddRange("Q", "W", "E");
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr1[1]);
+            //Console.WriteLine(arr1[2]);
+            //Console.WriteLine(arr1[3]);
+            //Console.WriteLine(arr1[4]);
+            //Console.WriteLine(arr1[5]);
+            //Console.WriteLine(arr1[6]);
+            //Console.WriteLine(arr1[7]);
+            //Console.WriteLine(arr1[8]);
+
+            //List<string> Name = new List<string>
+            //{ "王帅","刘帅","李帅" };
+            //Random r1 = new Random();
+            //int r2   = r1.Next(3);
+            //Console.WriteLine(Name[r2]);
+            //List<string> name = new List<string>
+            //  { "王帅","刘帅","李帅"};
+            //  name.Add("黄帅");
+            //  name.RemoveAt(1);
+            //  Console.WriteLine(name[0]);
+            //  Console.WriteLine(name[1]);
+            //  Console.WriteLine(name[2]);
+            //  Dictionary<string, dynamic> student = new Dictionary<string, dynamic>
+            //  {
+            //      ["name"] = "王帅",
+            //      ["age"] = "18",
+            //  };
+            //Console.WriteLine(student["age"]);
+            //  student["爱好"] = "篮球";
+            //  Console.WriteLine(student["爱好"]);
+            //var good = new
+            //{
+            //    name = "耳机",
+            //    money = "128",
+            //};
+            // Console.WriteLine(good.money);
+            //Console.WriteLine(good.name);
+
+
+        }
+    }
+}
