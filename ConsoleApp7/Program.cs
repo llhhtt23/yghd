60460c3a8c9da0c8bdccbc2403dc51150a85b14d
Your Name <you@example.com>
2026-08-10 20:45:36 +0800

    7

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string oldstr = "you love i";
+            //string[] strarr = oldstr.Split();
+            //List<string> strList = new();
+            //foreach (string str in strarr) strList.Add(str);
+            //strList.Reverse();
+            //string resStr = "";
+            //foreach (string item in strList)
+            //{
+            //    string firstLetter = item.Substring(0, 1).ToUpper();
+            //    string otherLetters = item.Substring(1).ToLower();
+            //    resStr += firstLetter + otherLetters + " ";
+            //}
+            //Console.WriteLine(resStr.Substring(0,resStr.Length-1));
+
+            //string str = "我叫王帅你记住，王晒太晒";
+            //List<string> mgc = ["王帅","王晒"];
+
+            //foreach (string str1 in mgc)
+            //{
+            //    string newStr = "";
+            //    for (int i = 0; i < str1.Length; i++) newStr += "*";
+            //    str = str.Replace(str1, newStr);
+            //}
+            //Console.WriteLine(str);
+
+            //string str = "hello,i am 王小帅，your name is 黄建?";
+
+            //MatchCollection mz = Regex.Matches(str, @"[\u4e00-\u9fa5]{2,3}");
+
+            //foreach (Match item in mz)
+            //{
+            //    Console.WriteLine(item.Value);
+            //}
+
+            //string str = "abc dd ee ff gg hh h j k";
+
+            //string res = Regex.Replace(str,@"\s+", " ");
+            //{  Console.WriteLine(res);
+            //}
+
+
+            //string str = "我的身份证号是: 360731200111052112,你的身份证是: 42108320041119211X";
+            //string p =@"\d{6}(\d{4})(\d{2})(\d{2})\d{3}[\dX]";
+
+            //MatchCollection mc = Regex.Matches(str, p);
+            //foreach (Match m in mc)
+            
+            //    {
+            //        Console.WriteLine("身份证：" + m.Groups[0].Value);
+            //        string year = m.Groups[1].Value;
+            //        string month = m.Groups[2].Value;
+            //        string day = m.Groups[3].Value;
+            //        Console.WriteLine($"出生{year}年{month}月{day}日");
+            //    }
+
+
+            
+
+
+        }
+    }
+}
